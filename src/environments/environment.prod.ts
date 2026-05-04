5e7177bd38d4383c446d94c6575ca4857052f81a
adelalrafiq <adelalrafiq@gmail.com>
2026-05-04 10:02:10 +0200

    fix bundle size

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     api: {
         baseUrl: 'https://traintracker-1.onrender.com/api',
         liveboard: '/liveboard',
+        stations: '/stations',
         connections: '/connections'
     },
     mapboxToken: 'pk.eyJ1IjoiYWRlbGFscmFmaXEiLCJhIjoiY2x4MDEwdmUyMDJ3YTJrczd2YmRvbGc1eiJ9.JUtRj0PX_ZSKzBvt9xagMg'
